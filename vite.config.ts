b9eefb25fcb7df83c720e0f49a07713b65c4a50c
Vladimir Yashin <yashin2236@gmail.com>
2025-03-29 17:44:57 +0300

    Установка Vitest + React Testing Library

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 import svgr from 'vite-plugin-svgr'
 
@@ -12,5 +12,11 @@ export default defineConfig({
         additionalData: '@use "/src/styles/global/mixins.scss" as *;'
       }
     }
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/setupTests.ts',
+    watch: false,
   }
 })
